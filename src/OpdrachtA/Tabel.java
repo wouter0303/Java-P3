71035b97a9902d8f3927f25044be979b75a1e42e
woute <woute@Wouter-laptop>
2019-03-28 11:14:04 +0100

    extra

--- src/OpdrachtA/Tabel.java
+++ src/OpdrachtA/Tabel.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Tabel {
 	public static void main(String[] args) {
 
-				for (int rij=0;rij<=9999;rij++) {
+				for (int rij=0;rij<=99999;rij++) {
 
 
 					for (int kolom=0;kolom<=9;kolom++) {
 						Random rand = new Random();
-						int n = rand.nextInt(10000);
+						int n = rand.nextInt(100000);
 						System.out.print(String.format("%8d,", n += 1));
 						
 					}
